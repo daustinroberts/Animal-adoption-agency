e93a0be2a4a67565a9b2da903d4fc2d44faefd40
Dennise Ortega, Jimmy McNamara and Kristi Hwang <dennise+jimmy+kristi@>
2018-04-09 11:01:27 -0700

    add Pet object with parameters

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+//backend
+function Pet (age, breed, neutered, friendly, exercise, availability){
+  this.age= age;
+  this.breed= breed;
+  this.neutered= neutered;
+  this.friendly= friendly;
+  this.exercise= exercise;
+}
