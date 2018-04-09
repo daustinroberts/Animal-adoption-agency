7c0547fb64cd46b7077d2af0d8ad918007bd2504
Dennise Ortega, Jimmy McNamara and Kristi Hwang <dennise+jimmy+kristi@>
2018-04-09 11:07:39 -0700

    add pet name to object  parameters

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 //backend
-function Pet (age, breed, neutered, friendly, exercise, availability){
+function Pet (name, age, breed, neutered, friendly, exercise, availability){
+  this.name= name;
   this.age= age;
   this.breed= breed;
   this.neutered= neutered;
