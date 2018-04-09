dcaa36d9e538139a29894a3dd74c47201053add2
Jimmy McNamara <jmcnamara44@gmail.com>
2018-04-09 11:08:34 -0700

    show animal info when animal picture is clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,19 @@
 //user interface logic
 
 $(document).ready(function() {
+  $("animal").click(function () { //need to update 'animal' with whatever the div is for each animal
+    $("animal-info").show();
+  });
+
   $("adopted").click(function() { //need to update 'adopted' with whatever the div or id is for adopted animals
     $("adopted").show();
   });
 
-  $("not-adopted").click(function() { //need to update 'adopted' with whatever the div or id is for adopted animals
+  $("not-adopted").click(function() { //need to update 'not-adopted' with whatever the div or id is for adopted animals
     $("not-adopted").show();
   });
 
-  $("submit").submit(function(event) {  //need to put in the submit button id/div
+  $("submit").submit(function(event) {  //need to put in the 'submit' button id/div
     event.preventDefault();
   });
 });
